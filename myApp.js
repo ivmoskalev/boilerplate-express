5b1161d53bf0877d11da3ad19cb9c8c638abd27a
Igor Moskalev <moskalev.igor.v@gmail.com>
2023-10-01 19:04:43 +0600

    Add logger middleware

--- myApp.js
+++ myApp.js
@@ -2,12 +2,15 @@ require('dotenv').config();
 let express = require('express');
 let app = express();
 
+app.use((req, res, next) => {
+    console.log(`${req.method} ${req.path} - ${req.ip}`);
+    next();
+});
 app.get("/", (req, res) => res.sendFile(__dirname + "/views/index.html"));
 app.use("/public", express.static(__dirname + "/public"));
 app.get("/json", (req, res) => process.env.MESSAGE_STYLE === "uppercase"
                              ? res.json({"message": "HELLO JSON"})
                              : res.json({"message": "Hello json"}));
-
 
 
 
