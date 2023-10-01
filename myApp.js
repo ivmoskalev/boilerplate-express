1350105aa82451316a66a9b5a0124dc88b67c383
Igor Moskalev <moskalev.igor.v@gmail.com>
2023-10-01 17:59:51 +0600

    Ex3. Serve an HTML File

--- myApp.js
+++ myApp.js
@@ -1,7 +1,7 @@
 let express = require('express');
 let app = express();
 
-app.get("/", (req, res) => res.send("Hello Express"))
+app.get("/", (req, res) => res.sendFile(__dirname + "/views/index.html"));
 
 
 
