7318252f99db1aa43c5de7a23ffe8f538c81a299
Igor Moskalev <moskalev.igor.v@gmail.com>
2023-10-01 18:26:43 +0600

    Ex5. Serve JSON

--- myApp.js
+++ myApp.js
@@ -3,7 +3,7 @@ let app = express();
 
 app.get("/", (req, res) => res.sendFile(__dirname + "/views/index.html"));
 app.use("/public", express.static(__dirname + "/public"));
-
+app.get("/json", (req, res) => res.json({"message": "Hello json"}));
 
 
 
