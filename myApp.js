0dda8808e36fad6027ef4a63a3d9b6b410485c77
Igor Moskalev <moskalev.igor.v@gmail.com>
2023-10-02 00:57:09 +0600

    Ex12. Add post request

--- myApp.js
+++ myApp.js
@@ -20,7 +20,11 @@ app.get("/now", (req, res, next) => {
     res.json({time: req.time})
 );
 app.get("/:word/echo", (req, res) => res.json({"echo": req.params.word}));
-app.route("/name").get((req, res) => res.json({"name": `${req.query.first} ${req.query.last}`}));
+app.route("/name").get(
+    (req, res) => res.json({"name": `${req.query.first} ${req.query.last}`})
+    ).post(
+        (req, res) => res.json({"name": `${req.body.first} ${req.body.last}`})
+    );
 
 
 
